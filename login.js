ff68c4e1bae98c6818933fdfc23671a4120a98a2
soujiro27 <jc.raus27@gmail.com>
2016-02-03 10:50:12 -0600

    validaciones de registro

--- login.js
+++ login.js
@@ -1,11 +1,12 @@
 var usuario="sabi";
 var contra="dorados";
-
+var numeros=['0','1','2','3','4','5','6','7','8','9'];
 $(document).on('ready',inicia);
 
 function inicia()
 {
-  $('input[type=submit]').on('click',validar);
+  //$('input[type=submit]').on('click',validar);
+  $('input#registro').on('click',registrar);
 }
 
 function validar(e)
@@ -19,11 +20,45 @@ function validar(e)
   }
   else
   {
-    $('div').append('<a href="registro.html">Registrate</a>');
+    $('div').append
+    ('<a href="registro.html">Registrate</a>');
   }
 }
 
 function redi()
 {
 window.location="http://www.tentaculos.com";
+}
+
+
+function registrar(e)
+{
+  e.preventDefault();
+  var nombre=$('input[type=text]').val();
+  var contra=$('input[type=password]').val();
+  //console.log(nombre[0]);
+  for(x in nombre)
+  {
+    for(y in numeros)
+    {
+      if(nombre[x]==numeros[y]){
+        console.log("no deben de ir numeros");
+      }
+
+    }
+
+  }
+
+var contador=0;
+for(x in contra)
+{
+  contador+=1;
+}
+if(contador<6 || contador>8)
+{
+  console.log("contraseña mal");
+}
+
+
+
 }
