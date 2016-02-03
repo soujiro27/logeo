6141f0b22df77f5100fd25a7918a375bf9393b42
soujiro27 <jc.raus27@gmail.com>
2016-02-03 08:34:40 -0600

    inicio de la funcion validar

---
+++ login.js
@@ -0,0 +1,14 @@
+var usuario="sabi";
+var contra="dorados";
+
+$(document).on('ready',inicia);
+
+function inicia()
+{
+  $('input[type=submit]').on('click',validar);
+}
+
+function validar()
+{
+  
+}
