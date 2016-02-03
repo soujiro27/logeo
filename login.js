67a6fb96f9cfea4a5a4e86c586f7a736de9a7753
soujiro27 <jc.raus27@gmail.com>
2016-02-03 09:15:37 -0600

    login terminado

--- login.js
+++ login.js
@@ -8,7 +8,22 @@ function inicia()
   $('input[type=submit]').on('click',validar);
 }
 
-function validar()
+function validar(e)
 {
-  
+  e.preventDefault();
+  var nombre=$('input[type=text]').val();
+  var pass=$('input[type=password]').val();
+  if(nombre==usuario && pass==contra)
+  {
+    setTimeout("redi()",5000);
+  }
+  else
+  {
+    $('div').append('<a href="registro.html">Registrate</a>');
+  }
+}
+
+function redi()
+{
+window.location="http://www.tentaculos.com";
 }
